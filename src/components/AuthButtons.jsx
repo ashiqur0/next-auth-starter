0a8ad52f6dc3b7264803780ffb660ba53ba01580
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-26 05:05:30 +0600

    Make Auhentication Login, Resister, And Logout Button Conditional Appearing With The Help Of useSession Hook

--- src/components/AuthButtons.jsx
+++ src/components/AuthButtons.jsx
@@ -2,13 +2,21 @@
 
 import Link from "next/link";
 import LoginButton from "./LoginButton";
+import { useSession } from "next-auth/react";
 
 const AuthButtons = () => {
+
+    const session = useSession();
+
     return (
         <div>
             <div className="flex gap-5">
-                <LoginButton />
-                <Link href={"/register"} className="btn">Register</Link>
+                {
+                    session.status == 'authenticated' ? <button className="btn">Log out</button> : <>
+                        <LoginButton />
+                        <Link href={"/register"} className="btn">Register</Link>
+                    </>
+                }
             </div>
         </div>
     );
