7a5dc43d862a0f6f0584a54e89b0169f66d30d35
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-26 05:42:20 +0600

    Create Private Page

---
+++ src/app/private/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivatePage = () => {
+    return (
+        <div className='min-h-screen bg-slate-800'>
+            <h1 className='text-4xl font-bold text-center py-10'>Private</h1>
+        </div>
+    );
+};
+
+export default PrivatePage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Navbar = () => {
 
     return (
-        <nav className='bg-slate-950'>
+        <nav className='bg-slate-800'>
             <div className='py-3 w-7xl mx-auto flex items-center justify-between'>
                 <div>Next Auth</div>
                 <div className='flex items-center gap-3'>
