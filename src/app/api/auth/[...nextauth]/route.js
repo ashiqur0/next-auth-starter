acb15f4ea2f64cc5d04d3291b36e50ec97b4a2f8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-25 16:47:00 +0600

    Building Authentication System Using Next Auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth"
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        // ...add more providers here
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+
+            // form inputs
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // my own login logic
+                
+
+                return null;
+            }
+        })
+    ],
+}
+
+export default NextAuth(authOptions)
